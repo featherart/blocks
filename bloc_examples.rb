3c7d88d6d8354ffb96ce11e04959f96b2c0776b8
Feather <lisafeather@gmail.com>
2013-08-27 15:15:21 -0400

    update block

--- bloc_examples.rb
+++ bloc_examples.rb
@@ -3,12 +3,10 @@
 
 # basic block syntax differs from lambdas in that there is no "lamdba"
 # other than that, what is different?
-# for one thing there is the word "def" 
-def bank_block(number)
-	return "There are " + number.to_s + " dollars in my bank account"
-end
 
-# call with bank_block(90)
+i = "hello"
+3.times { |i| puts i }
 
-# or you can call with a block of code!
+
+# call with puts i
 
